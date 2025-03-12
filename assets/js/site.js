5bafcd934547812165a80f9eb948f41c97fe846f
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-12 12:59:34 +0100

    Change #2

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,23 +1,22 @@
-// write cool JS here!!
-console.time()
-const myInfo = 'en variabel';
-const myBooks = ["The Catcher in the Rye",
-    "To Kill a Mockingbird",
-    "1984",
-    "Pride and Prejudice",
-    "The Great Gatsby",
-    "The Hobbit",
-    "Harry Potter and the Sorcerer's Stone",
-    "The Lord of the Rings",
-    "The Da Vinci Code",
-    "The Hunger Games"]
 
-console.clear()     
-console.group('Konsol opgave')    
-console.log('Jeg er en almindelig konsol log');
-console.info('Jeg er en konsol info:', myInfo)
-console.warn('jeg er en advarsel!')
-console.error('jeg er en fejl!!')
-console.table(myBooks)
-console.groupEnd()
-console.timeEnd()
+function ChangeH1(myText) {
+    const myElement = document.getElementById('headline')
+    myElement.innerText = myText; 
+}
+
+
+
+
+// hvad gør funk
+// function addValues(valueOne,valueTwo){
+
+//     let result = valueOne + valueTwo;
+//     return result;
+// }
+
+// let AddedValue = addValues(10, 50);
+
+// let anotherAddedValue = addValues(10000, 800);
+
+// console.log(AddedValue);
+// console.log(anotherAddedValue);
