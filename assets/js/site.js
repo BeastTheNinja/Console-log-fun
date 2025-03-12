8204dd0507e728241eb37795d08c6e8f031397cc
BeastTheNinja <134609528+BeastTheNinja@users.noreply.github.com>
2025-03-12 11:19:52 +0100

    Change #1

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,5 @@
 // write cool JS here!!
+console.time()
 const myInfo = 'en variabel';
 const myBooks = ["The Catcher in the Rye",
     "To Kill a Mockingbird",
@@ -11,3 +12,12 @@ const myBooks = ["The Catcher in the Rye",
     "The Da Vinci Code",
     "The Hunger Games"]
 
+console.clear()     
+console.group('Konsol opgave')    
+console.log('Jeg er en almindelig konsol log');
+console.info('Jeg er en konsol info:', myInfo)
+console.warn('jeg er en advarsel!')
+console.error('jeg er en fejl!!')
+console.table(myBooks)
+console.groupEnd()
+console.timeEnd()
